de1d14a0c4f11ff564d2a748e6a2e1e10a580e6e
BRANDON LUM <brandonlum17@gmail.com>
2024-10-24 17:50:55 -0400

    feat:setup dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -6,6 +7,7 @@ const app = express();
 const { PORT = 3001 } = process.env;
 const { errors } = require("celebrate");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+
 
 app.use(cors());
 const mainRouter = require("./routes/index");
