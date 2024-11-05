d4bba7edc44cb3f7d06611fd4e09e1e5f16911f5
bryzle <brandonlum17@gmail.com>
2024-11-05 09:13:41 -0500

    chore: Improve authorization error handling and use environment variable for JWT_SECRET

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,19 +4,19 @@ const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
+  const error = new Error(ERROR_MESSAGES.AUTHORIZATION_ERROR);
   if (!authorization || !authorization.startsWith("Bearer")) {
-    return res
-      .status(ERROR_CODES.AUTHORIZATION_ERROR)
-      .json({ message: ERROR_MESSAGES.AUTHORIZATION_ERROR });
+
+      error.status = ERROR_CODES.AUTHORIZATION_ERROR;
+      return next(error);
   }
   const token = authorization.replace("Bearer ", "");
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res
-      .status(ERROR_CODES.AUTHORIZATION_ERROR)
-      .send({ message: ERROR_MESSAGES.AUTHORIZATION_ERROR });
+    error.status = ERROR_CODES.AUTHORIZATION_ERROR;
+    return next(error);
   }
 
    (req.user = payload);
