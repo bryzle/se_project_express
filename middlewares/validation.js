629ec0752d3550883f50438c2d30d784959a8f85
BRANDON LUM <brandonlum17@gmail.com>
2024-11-14 10:22:20 -0500

    Fixes to eslint errors. Corrected notfound middleware. Added name and avatar parameters in validateuserlogin validation.

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -15,7 +15,7 @@ module.exports.validateClothingItem = celebrate({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'the "imageUrl" field must be a valid url',
     }),
-    weather: Joi.string().valid('hot', 'warm', 'cold').required(),
+    weather: Joi.string().valid("hot", "warm", "cold").required(),
   }),
 });
 
@@ -33,7 +33,12 @@ module.exports.validateNewUser = celebrate({
 
 module.exports.validateUserLogin = celebrate({
   body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().custom(validateURL).messages({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'the "imageUrl" field must be a valid url',
+    }),
+    avatar: Joi.string().custom(validateURL).messages({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'the "imageUrl" field must be a valid url',
     }),
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const user = require("../controllers/users");
 const auth = require("../middlewares/auth");
-const {validateId} = require('../middlewares/validation')
 
-const { getCurrentUsers, getUser, updateUser  } = user;
+
+const { getCurrentUsers, updateUser  } = user;
 
 
 router.get("/me", auth, getCurrentUsers);
